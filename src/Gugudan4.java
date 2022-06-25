f29ea6498e146acafc517e64afaf2c1ef6faa579
82108 <82108@DESKTOP-D518FSA>
2022-06-25 17:03:27 +0900

    완성

---
+++ src/Gugudan4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Gugudan4 {
+    public static void main(String[] args) {
+        System.out.println("구구단 중 출력할 단은? : ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        
+        if (number < 2) {
+            System.out.println("2이상, 9이하의 값만 입력할 수 있습니다.");
+        } else if (number > 9) {
+        	System.out.println("2이상, 9이하의 값만 입력할 수 있습니다.");
+            // 사용자에게 에러 메시지 출력
+        } else {
+        	for (int i = 1; i <10; i++) {
+            	System.out.println(number * i);
+        }
+       
+        }
+    }
+}
