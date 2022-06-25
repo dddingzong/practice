b79f25555224cb50478dede40a708e9d9ad38526
82108 <82108@DESKTOP-D518FSA>
2022-06-25 15:39:45 +0900

    완성

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,4 +1,26 @@
 
 public class Gugudan {
-
+	public static void main(String[] args) {
+		int x = 2;
+		int y = 3;
+		//2단
+		System.out.println(x*2);
+		System.out.println(x*3);
+		System.out.println(x*4);
+		System.out.println(x*5);
+		System.out.println(x*6);
+		System.out.println(x*7);
+		System.out.println(x*8);
+		System.out.println(x*9);
+		
+		//3단
+		System.out.println(y*2);
+		System.out.println(y*3);
+		System.out.println(y*4);
+		System.out.println(y*5);
+		System.out.println(y*6);
+		System.out.println(y*7);
+		System.out.println(y*8);
+		System.out.println(y*9);	
+	}
 }
