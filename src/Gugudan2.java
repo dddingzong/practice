9dc8024d66463c375f0e6fad432c81961febe2bc
82108 <82108@DESKTOP-D518FSA>
2022-06-25 16:04:21 +0900

    완성

---
+++ src/Gugudan2.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+
+public class Gugudan2 {
+    public static void main(String[] args) {
+    	
+    	System.out.println("구구단 중 출력할 단은? : ");
+        Scanner scanner = new Scanner(System.in);
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+ 
+        int result = number1 * 1;
+        System.out.println(result);
+        result = number1 * 2;
+        System.out.println(result);
+        result = number1 * 3;
+        System.out.println(result);
+        result = number1 * 4;
+        System.out.println(result);
+        result = number1 * 5;
+        System.out.println(result);
+        result = number1 * 6;
+        System.out.println(result);
+        result = number1 * 7;
+        System.out.println(result);
+        result = number1 * 8;
+        System.out.println(result);
+        result = number1 * 9;
+        System.out.println(result);
+        
+        int result2 = number2 * 1;
+        System.out.println(result2);
+        result2 = number2 * 2;
+        System.out.println(result2);
+        result2 = number2 * 3;
+        System.out.println(result2);
+        result2 = number2 * 4;
+        System.out.println(result2);
+        result2 = number2 * 5;
+        System.out.println(result2);
+        result2 = number2 * 6;
+        System.out.println(result2);
+        result2 = number2 * 7;
+        System.out.println(result2);
+        result2 = number2 * 8;
+        System.out.println(result2);
+        result2 = number2 * 9;
+        System.out.println(result2);
+    }
+}
+
