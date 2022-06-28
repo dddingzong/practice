4db2fc9c3f29c410b26cb3f28c5552793d0b9f2e
82108 <82108@DESKTOP-D518FSA>
2022-06-28 19:10:03 +0900

    완성

---
+++ src/GugudanMain.java
@@ -0,0 +1,7 @@
+public class GugudanMain {
+    public static void main(String[] args) {
+        for (int j = 2; j < 10; j++) {
+             Gugudan7.print(Gugudan7.calculate(j));
+        }
+    }
+}
